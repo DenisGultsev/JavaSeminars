e9a64ed73dfb87aa551c1e232f6f86497d62dc31
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-11-09 22:07:18 +1000

    Hello

---
+++ Seminar1/Hello.java
@@ -0,0 +1,12 @@
+package Seminar1;
+
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String name = scan.nextLine();
+        System.out.println("Привет, " + name + "!");
+    }
+}
